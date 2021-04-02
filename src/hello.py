ddc4d7142f915371842cdc2545ba0ba83548ace6
tellnnn <tellurium3262@gmail.com>
2021-04-02 18:38:16 +0900

    modify hello.py

--- src/hello.py
+++ src/hello.py
@@ -1,6 +1,10 @@
 from psychopy import visual, core
 
-win = visual.Window()
-msg = visual.TextStim(win, text=u"\u00A1Hola mundo!")
-msg.draw()
+win = visual.Window([400,400])
+message = visual.TextStim(win, text='hello')
+message.autoDraw = True  # Automatically draw every frame
 win.flip()
+core.wait(2.0)
+message.text = 'world'  # Change properties of existing stim
+win.flip()
+core.wait(2.0)
